04ccea4972281060efbb327f6e36878c1c30f3e9
Lin Marc <marclin750@gmail.com>
2024-06-09 23:39:03 +0200

    change loop while to loop for in the file sin.py

--- sin.py
+++ sin.py
@@ -1,25 +1,21 @@
-nbrTour = 0
+
 n = 1
 res = 0
 
 print("Entrer un nombre pour cacluler approximativement son sinus: ", end='')
 data = float(input())
 
-while True:
+for j in range(12):
     fact = 1
     rad = data**n
     for i in range(2, n+1):
         fact = fact * i
 
-    if nbrTour%2 == 0:
+    if j%2 == 0:
         res = res + rad / fact
     else:
         res = res - rad / fact
-    
-    if nbrTour == 10:
-        break
 
-    nbrTour += 1
     n += 2
 
 print(res)
