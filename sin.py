981cbdeac8bbf5b6ec19261e8b0660270f8f846d
Lin Marc <marclin750@gmail.com>
2024-06-09 23:28:49 +0200

    modify code file sin.py

--- sin.py
+++ sin.py
@@ -1,5 +1,25 @@
-n = int(input())
+nbrTour = 0
+n = 1
+res = 0
 
-fact = 1
-for i in range(2, n+1):
-    fact = fact * i
+print("Entrer un nombre pour cacluler approximativement son sinus: ", end='')
+data = float(input())
+
+while True:
+    fact = 1
+    rad = data**n
+    for i in range(2, n+1):
+        fact = fact * i
+
+    if nbrTour%2 == 0:
+        res = res + rad / fact
+    else:
+        res = res - rad / fact
+    
+    if nbrTour == 10:
+        break
+
+    nbrTour += 1
+    n += 2
+
+print(res)
